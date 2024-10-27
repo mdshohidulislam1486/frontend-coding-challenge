0bc856e51096ec4088f9d8aa69194cdd28b2ed65
mdshohidulislam1486 <mdshohidulislam1486@gmail.com>
2024-10-27 22:23:45 +0600

    user-agent-used

--- src/components/providers/providers.tsx
+++ src/components/providers/providers.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { UserAgentProvider } from './userAgentProvider';
-import { userAgent as getUserAgent } from 'next/server';
+import { userAgent } from 'next/server';
 import { headers } from 'next/headers';
 
 export const Providers: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
-  const { ua } = getUserAgent({ headers: headers() });
+  const { ua } = userAgent({ headers: headers() });
   return <UserAgentProvider userAgent={ua}>{children}</UserAgentProvider>;
 };
